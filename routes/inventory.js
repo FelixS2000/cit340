2c093527e52051c2b58a7d7ec5804dd6b3099cf7
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2025-01-21 14:57:55 +0000

    saing

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,3 @@
-// routes/inventory.js
-
-const express = require('express');
-const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
-
-// Route to get vehicle details by ID
-router.get('/inventory/:id', inventoryController.getVehicleDetails);
-router.get('/detail/:id', inventoryController.getVehicleDetails);
-
-module.exports = router;
+import {buildVehicleHTML} from "../utilities/index"
+const html = buildVehicleHTML;
+return html;
