bc2fb04a036484525244c51a3bdbfffa5488052b
Felix Samuel Guzman Ceri <113263790+FelixS2000@users.noreply.github.com>
2025-01-21 23:06:40 +0000

    updates

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg'); // Assuming PostgreSQL is being used
 
 const pool = new Pool({
     user: 'cse340dbguzman', // Replace with your database username
-    host: 'dpg-cu55pnggph6c73du20i0-a', // Replace with your database host
+    host: 'render.com', // Replace with your database host
     database: 'cse340dbguzman', // Replace with your database name
     password: 'K5RxwheTudWT21nsfYlA0rxoYGApEm7J', // Replace with your database password
     port: 5432, // Replace with your database port
